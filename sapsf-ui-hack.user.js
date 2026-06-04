cac37a4119967bae3764400d88d76b03df161242
toooma <tmurvai@gmail.com>
2026-06-04 15:57:39 +0200

    .

--- sapsf-ui-hack.user.js
+++ sapsf-ui-hack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SAP SuccessFactors UI Hack
 // @namespace    https://github.com/toooma/sapsf-ui-hack
-// @version      0.1.5
+// @version      0.1.6
 // @description  Enhances SAP SuccessFactors UI.
 // @match        https://hcm55.sapsf.eu/*
 // @run-at       document-start
