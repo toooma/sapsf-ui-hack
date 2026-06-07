239cd4228dd3952fe23384b07f4763b4f34a145c
toooma <tmurvai@gmail.com>
2026-06-07 14:12:56 +0200

    .

--- sapsf-ui-hack.user.js
+++ sapsf-ui-hack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SAP SuccessFactors UI Hack
 // @namespace    https://github.com/toooma/sapsf-ui-hack
-// @version      0.8.3
+// @version      0.8.4
 // @description  Enhances SAP SuccessFactors UI.
 // @match        https://hcm55.sapsf.eu/*
 // @run-at       document-end
@@ -185,6 +185,10 @@
       [style*="visibility:hidden"] {
         display: none !important;
       }
+      .emptyActionItem {
+        display: none !important;
+      }
+
     `;
 
     document.documentElement.appendChild(style);
